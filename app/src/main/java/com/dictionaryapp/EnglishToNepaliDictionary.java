c4a7de6c2b032c29503a139152c69555c2977158
Softwarica College Lab <softwarica@softwarica.com>
2019-04-25 11:39:16 +0545

    added word in dictionary

--- app/src/main/java/com/dictionaryapp/EnglishToNepaliDictionary.java
+++ app/src/main/java/com/dictionaryapp/EnglishToNepaliDictionary.java
@@ -6,35 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EnglishToNepaliDictionary extends AppCompatActivity {
 
+    Button btnAdd;
     private ListView lstDictionary;
     private Map<String, String> dictionary;
-    public static final String words[] = {
-            "timro naam k ho", "What is your name",
-            "mero ghar gorkha ho", "My home is Gorkha",
-            "ma kta ho", "I am a boy",
-            "mero aauta sathi xa", "I have a friend",
-            "mero naam rojin ho", "my name is Rojin"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english_to_nepali_dictionary);
 
+//        btnAdd=findViewById(R.id.btnAdd);
+
+//        btnAdd.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(EnglishToNepaliDictionary.this, AddWordActivity.class);
+//
+//                startActivity(intent);
+//            }
+//        });
         lstDictionary = findViewById(R.id.lstDictionary);
         dictionary = new HashMap<>();
-        for (int i = 0; i < words.length; i += 2) {
-            dictionary.put(words[i], words[i + 1]);
-        }
-        ArrayAdapter english = new ArrayAdapter<>(this,
+//        for (int i = 0; i < words.length; i += 2) {
+//            dictionary.put(words[i], words[i + 1]);
+//        }
+
+        readFromFile();
+        ArrayAdapter english = new ArrayAdapter<>(
+                this,
                 android.R.layout.simple_list_item_1,
                 new ArrayList<String>(dictionary.keySet())
         );
@@ -55,4 +67,22 @@ public class EnglishToNepaliDictionary extends AppCompatActivity {
 
 
     }
+
+      private void readFromFile() {
+        try{
+            FileInputStream fos=openFileInput("words.txt");
+            InputStreamReader isr =new InputStreamReader(fos);
+            BufferedReader br =new BufferedReader(isr);
+            String line="";
+            while ((line=br.readLine())!=null){
+                String [] parts=line.split(">");
+                dictionary.put(parts[0], parts[1]);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
